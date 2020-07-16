cb721609565e1dbc95ad930ce8af800675573294
PedroAugusto <nerdindiano@gmail.com>
2020-07-16 10:14:54 -0300

    first commit from recycle colect points project

---
+++ jsfofo.js
@@ -0,0 +1,37 @@
+// let produtos = [
+//   { nome: "Notebook", valor: 2500 },
+//   { nome: "Monitor", valor: 900 },
+//   { nome: "Mouse e teclado", valor: 100 },
+// ];
+
+// let valorTotal = 0;
+
+// for (let i = 0; i < produtos.length; i++) {
+//   valorTotal += produtos[i].valor;
+// }
+// console.log(valorTotal);
+
+// let distancia = 20;
+// let quantidadeTanque = 50;
+// let LitrosPorKm = 2;
+
+// for (let i = 0; i < distancia; i++) {
+//   quantidadeTanque -= LitrosPorKm;
+// }
+
+// console.log(quantidadeTanque);
+
+let jogos = [
+  { nome: "Call of Duty", classificacao: 18 },
+  { nome: "Resident Evil 6", classificacao: 16 },
+  { nome: "Super Bomberman R", classificacao: 7 },
+  { nome: "Bomberman", classificacao: 7 },
+];
+
+let idade = 15;
+
+for (let i = 0; i < jogos.length; i++) {
+  if (idade >= jogos.classificacao) {
+    console.log(jogos.nome.value);
+  }
+}
